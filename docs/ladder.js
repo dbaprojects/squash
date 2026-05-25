94c8d9011fad594697b40996ad59d35a4288de50
David Barkess <dbarkess@gmail.com>
2026-05-25 16:34:33 +0800

    ladder: add rules list to banner; rule 4 uses dynamic challenge range

--- docs/ladder.js
+++ docs/ladder.js
@@ -188,6 +188,12 @@ function renderDivisionLadder() {
   wrap.innerHTML = `
     <div class="ladder-banner">
       <strong>Throw down a challenge!</strong> Ladders are currently updated by David B., so let him know if any movements.
+      <ol class="ladder-rules">
+        <li>Winner takes loser's place. Loser goes down 1 place.</li>
+        <li>Can't refuse a challenge (else you slip a place!)</li>
+        <li>Not required to do more than 1 challenge per session.</li>
+        <li>You can challenge up to <strong>${_challengeRange}</strong> place${_challengeRange !== 1 ? 's' : ''} above you.</li>
+      </ol>
     </div>
     <div class="div-ladder-grid">${divCards.join('')}</div>`;
 }
